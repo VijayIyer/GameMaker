e48b6ffd5827f943cf5b88fcca79b25b0f0ea603
vijay <VijayIyer>
2022-09-17 22:05:59 -0400

    constants.ts file

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,10 @@
+import { Circle } from "./model/circle_sprite.js";
 import { ImageSprite } from "./model/image_sprite.js";
+import { Rectangle } from "./model/rectangle_sprite.js";
 import { SoundSprite } from "./model/sound_sprites.js";
 import { Sprite } from "./model/sprite.js";
-import { TextSprite } from "./model/text_sprites.js";
+import { ClockSprite, TextSprite } from "./model/text_sprites.js";
 
-export const presetSprites:Array<Sprite> = [new ImageSprite(),
+export const presetSprites:Array<Sprite> = [new Circle(10, 10, "red", 5), new Rectangle(0, 0, "blue", 10, 10), 
      new SoundSprite(),
-     new TextSprite("Content")];
+     new TextSprite("Content", 10, 10, 10, "20px Arial"), new ClockSprite()];
